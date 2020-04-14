b315c97165a061730d26060c64f95015aac74c0b
Nyawira Muturi <albinamuturi@gmail.com>
2020-04-14 17:52:17 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent},
+  {path: '', redirectTo:"/profile", pathMatch:"full"},
 
-const routes: Routes = [];
+  { path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
